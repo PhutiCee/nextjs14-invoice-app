44339406478ed67a1e9bdd8fbbdfd3881cd25326
PhutiCee <trymichelsekwadi@gmail.com>
2024-08-04 15:51:51 +0200

    added footer and priority on sitemap

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import InvoiceForm from '../components/InvoiceForm'
+import Footer from '../components/footer/Footer'
 // import Invoices from '../components/Invoices'
 
 export default function dashboard() {
     return (
         <div className="p-5">
             <InvoiceForm />
+            <Footer />
         </div>
     )
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,13 +7,13 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: baseUrl,
       lastModified: new Date(),
       changeFrequency: 'yearly',
-      priority: 0.8,
+      priority: 1,
     },
     {
       url: `${baseUrl}/dashboard`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 1,
+      priority: 0.8,
     },
   ]
 }
